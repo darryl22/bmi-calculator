a3b7b549e9dec440430d309eedb7e60360c782ff
Darryl A. Mbatia <dmbatia@usiu.ac.ke>
2022-03-15 11:57:48 +0300

    added procfile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
 
@@ -28,4 +28,4 @@ app.post('/bmi-results', urlEncodedParser, function (request, response){
 });
 
 app.listen(port);
-console.log('Server is listening on port 3000');
+console.log(`Server is listening on port ${port}`);
